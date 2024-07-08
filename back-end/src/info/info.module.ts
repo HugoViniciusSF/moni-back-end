680e0f24a51e30e941ee1f3a3cb58f3a644a51b7
GustavoRocha3264 <onlyakawaii@gmail.com>
2024-07-08 19:28:19 -0300

    info adjustements

--- back-end/src/info/info.module.ts
+++ back-end/src/info/info.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { InfoController } from './info.controller';
 import { InfoServiceProvider } from './info.service';
 import { HttpModule } from '@nestjs/axios';
-import { InfoGatheringFacadeProviderJogos } from './providers/info.gathering.service.jogos';
 import { ConfigModule } from '@nestjs/config';
-import { InfoGatheringFacadeProviderQuestoes } from './providers/info.gathering.service.questoes';
-import { InfoGatheringFacadeProviderNoticias } from './providers/info.gathering.service.noticias';
+import { InfoGatheringFacadeQuestoes } from './providers/info.gathering.service.questoes';
+import { InfoGatheringFacadeNoticias } from './providers/info.gathering.service.noticias';
+import { InfoGatheringFacadeJogos } from './providers/info.gathering.service.jogos';
 
 @Module({
   imports: [
@@ -15,9 +15,9 @@ import { InfoGatheringFacadeProviderNoticias } from './providers/info.gathering.
   controllers: [InfoController],
   providers: [
     InfoServiceProvider,
-    //InfoGatheringFacadeProviderQuestoes,
-    InfoGatheringFacadeProviderJogos,
-    //InfoGatheringFacadeProviderNoticias,
+    //InfoGatheringFacadeQuestoes,
+    //InfoGatheringFacadeJogos,
+    InfoGatheringFacadeNoticias,
   ],
 })
 export class InfoModule { }
